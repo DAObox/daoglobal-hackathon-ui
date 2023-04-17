c1f4744788aa3f00914e75337a4b93198e632b10
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-04-18 00:27:48 +0100

    members count card complete

--- src/components/members/MemberCardList.tsx
+++ src/components/members/MemberCardList.tsx
@@ -1,26 +1,13 @@
-import { pluginAddress } from "@constants/daoConfig";
 import MemberCard from "./MemberCard";
-import {  useFetchMembersAndBalances } from '@daobox/use-aragon'
-import Loading from "./Loading";
-import Error from "./Error";
+import { mockMembersCards } from "@constants/mocks/MockMemberCards";
 
 export default function MemberCardList() {
-    const { data, isLoading, isError } = useFetchMembersAndBalances({
-        pluginAddress: pluginAddress,
-    })
-    if (isLoading) {
-        return <div className="w-full mt-20 flex justify-center"><Loading /></div>
-    }
-    else if (isError) {
-        return <div className="w-full"><Error /></div>
-    }
-    else {
-        return(
-            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-3">
-                {
-                    data?.map(item => <MemberCard avatar={""} address={item.address || ""} balance={(Number(item.balance)/10**18).toString()} power={""} />)
-                }
-            </div>
-        )
-    }
+    return(
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-3">
+            {
+                mockMembersCards?.map(item => <MemberCard avatar={item?.avatar} address={item.address || ""} 
+                balance={(Number(item.balance)/10**18).toString()} power={item.power} />)
+            }
+        </div>
+    )
 }
---
+++ src/constants/mocks/MockMemberCards.ts
@@ -0,0 +1,79 @@
+export const mockMembersCards = [
+    {
+        id: "1",
+        avatar: "/profile.png",
+        address: "0x5C04F69c9603A808BF4157Ef959F1Ed1e16c0F73",
+        balance: "2000000000000000000",
+        power: "2.07"
+    },
+    {
+        id: "1",
+        avatar: "/profile.png",
+        address: "0x5C04F69c9603A808BF4157Ef959F1Ed1e16c0F73",
+        balance: "2000000000000000000",
+        power: "2.07"
+    },
+    {
+        id: "1",
+        avatar: "/profile.png",
+        address: "0x5C04F69c9603A808BF4157Ef959F1Ed1e16c0F73",
+        balance: "2000000000000000000",
+        power: "2.07"
+    },
+    {
+        id: "1",
+        avatar: "/profile.png",
+        address: "0x5C04F69c9603A808BF4157Ef959F1Ed1e16c0F73",
+        balance: "2000000000000000000",
+        power: "2.07"
+    },
+    {
+        id: "1",
+        avatar: "/profile.png",
+        address: "0x5C04F69c9603A808BF4157Ef959F1Ed1e16c0F73",
+        balance: "2000000000000000000",
+        power: "2.07"
+    },
+    {
+        id: "1",
+        avatar: "/profile.png",
+        address: "0x5C04F69c9603A808BF4157Ef959F1Ed1e16c0F73",
+        balance: "2000000000000000000",
+        power: "2.07"
+    },
+    {
+        id: "1",
+        avatar: "/profile.png",
+        address: "0x5C04F69c9603A808BF4157Ef959F1Ed1e16c0F73",
+        balance: "2000000000000000000",
+        power: "2.07"
+    },
+    {
+        id: "1",
+        avatar: "/profile.png",
+        address: "0x5C04F69c9603A808BF4157Ef959F1Ed1e16c0F73",
+        balance: "2000000000000000000",
+        power: "2.07"
+    },
+    {
+        id: "1",
+        avatar: "/profile.png",
+        address: "0x5C04F69c9603A808BF4157Ef959F1Ed1e16c0F73",
+        balance: "2000000000000000000",
+        power: "2.07"
+    },
+    {
+        id: "1",
+        avatar: "/profile.png",
+        address: "0x5C04F69c9603A808BF4157Ef959F1Ed1e16c0F73",
+        balance: "2000000000000000000",
+        power: "2.07"
+    },
+    {
+        id: "1",
+        avatar: "/profile.png",
+        address: "0x5C04F69c9603A808BF4157Ef959F1Ed1e16c0F73",
+        balance: "2000000000000000000",
+        power: "2.07"
+    },
+]
