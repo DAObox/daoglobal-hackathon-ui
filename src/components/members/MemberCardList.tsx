4fde6eab7060d0118a918f4b19c026407330eb06
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-04-18 00:29:18 +0100

    members count card complete

--- src/components/members/MemberCardList.tsx
+++ src/components/members/MemberCardList.tsx
@@ -5,7 +5,7 @@ export default function MemberCardList() {
     return(
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-3">
             {
-                mockMembersCards?.map(item => <MemberCard avatar={item?.avatar} address={item.address || ""} 
+                mockMembersCards?.map(item => <MemberCard key={item?.id} avatar={item?.avatar} address={item.address || ""} 
                 balance={(Number(item.balance)/10**18).toString()} power={item.power} />)
             }
         </div>
