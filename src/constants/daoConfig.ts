4247e61be677efb463f4dcea40ec15a6e06c9b90
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-04-18 20:46:40 +0100

    members count complete

--- src/constants/daoConfig.ts
+++ src/constants/daoConfig.ts
@@ -1,2 +1,2 @@
 export const daoAddressOrEns = "0x6f07aa7af27e0e06a08a1a17e04c4b0eb11300ab";
-export const pluginAddress = "0x3520d16b3ca5a1a7e60a744630c65a603bb68953";
+export const pluginAddress = "0x8eaf189dbe3524667d25684645aba1c71c02d8db";
