b09fbc9cb69ba86c95150bf28d631b28f07ca3bf
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-04-28 15:07:02 +0100

    tokens

--- src/components/treasury/TreasuryTabs.tsx
+++ src/components/treasury/TreasuryTabs.tsx
@@ -5,12 +5,12 @@ export default function TreasuryTabs() {
     return(
         <div className="flex space-x-2.5">
             <button
-                className={`hover:underline hover:underline-offset-4 hover:decoration-daoboxg decoration-4 font-semibold 
-                ${tab == "tokens" && "underline underline-offset-4 decoration-daoboxg decoration-4"}`}
+                className={`hover:underline hover:underline-offset-8 hover:decoration-daoboxg decoration-4 font-semibold 
+                ${tab == "tokens" && "underline underline-offset-8 decoration-daoboxg decoration-4"}`}
                 onClick={() => setTab("tokens")}>Tokens</button>
             <button
-                className={`hover:underline hover:underline-offset-4 hover:decoration-daoboxg decoration-4 font-semibold 
-                ${tab == "transactions" && "underline underline-offset-4 decoration-daoboxg decoration-4"}`}
+                className={`hover:underline hover:underline-offset-8 hover:decoration-daoboxg decoration-4 font-semibold 
+                ${tab == "transactions" && "underline underline-offset-8 decoration-daoboxg decoration-4"}`}
                 onClick={() => setTab("transactions")}>Transactions</button>
         </div>
     )
