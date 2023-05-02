bf651db6232e7813747e7d2d2df60f2fffd1518a
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-05-02 23:47:38 +0100

    finance

--- src/components/treasury/TransactionsTab.tsx
+++ src/components/treasury/TransactionsTab.tsx
@@ -8,33 +8,6 @@ export default function TransactionsTab() {
 
   const [data, setData] = useState([])
 
-    const cities = [
-        {
-          city: "Athens",
-          rating: "2 open PR",
-        },
-        {
-          city: "Luzern",
-          rating: "1 open PR",
-        },
-        {
-          city: "Zürich",
-          rating: "0 open PR",
-        },
-        {
-          city: "Vienna",
-          rating: "1 open PR",
-        },
-        {
-          city: "Ermatingen",
-          rating: "0 open PR",
-        },
-        {
-          city: "Lisbon",
-          rating: "0 open PR",
-        },
-      ];
-
       let etherscanProvider = new ethers.providers.EtherscanProvider()
 
       etherscanProvider.getHistory(daoAddressOrEns).then((history: any) => {
